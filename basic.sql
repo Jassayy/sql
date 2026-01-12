b63401c160b40c4da683cdb476c1bc75fc663144
Jassayy <jasgandhi2004@gmail.com>
2026-01-12 22:26:41 +0530

    where clause with certain operators we can use

--- basic.sql
+++ basic.sql
@@ -1,2 +1,16 @@
-SELECT name , unit_price , unit_price * 1.1 AS 'new price'
-FROM products;
+-- the where clause
+SELECT *
+FROM customers
+-- WHERE points > 3000;
+-- WHERE state <> 'VA'; -- put string in ''
+-- WHERE state != 'VA';
+-- WHERE state = 'VA';
+WHERE birth_date = '2004-02-19';
+
+-- operators we can use in sql
+-- >
+-- <=
+-- <
+-- >=
+-- =
+-- != or <> we can use both of these for not equal
