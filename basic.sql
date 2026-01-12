e3818c874185ee17127d54621be60cabced6d5ed
Jassayy <jasgandhi2004@gmail.com>
2026-01-12 22:40:20 +0530

    excercise 2

--- basic.sql
+++ basic.sql
@@ -1,16 +1,6 @@
--- the where clause
-SELECT *
-FROM customers
--- WHERE points > 3000;
--- WHERE state <> 'VA'; -- put string in ''
--- WHERE state != 'VA';
--- WHERE state = 'VA';
-WHERE birth_date = '2004-02-19';
+-- excercise 2
+-- get orders placed this year (2017)
 
--- operators we can use in sql
--- >
--- <=
--- <
--- >=
--- =
--- != or <> we can use both of these for not equal
+SELECT * 
+FROM orders
+WHERE order_date >= '2019-01-01';
