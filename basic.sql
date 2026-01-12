b15bd7770dbacd67ac8acbe6dc81a00d562c9cb2
Jassayy <jasgandhi2004@gmail.com>
2026-01-12 22:12:39 +0530

    distinct keyword

--- basic.sql
+++ basic.sql
@@ -1,8 +1,5 @@
-SELECT first_name , last_name , phone , points , points + 10 AS 'some modifying in data', (points + 10) * 100 AS 'discount price'           
--- we can even use arithmetic operators and modify however we want
--- using AS keyword and then specify the column name for that particular data
--- if we wanna have space in that col name we must pass it inside ''
-FROM customers ;
+SELECT DISTINCT state
+FROM customers
 
-
-
+-- two rows have the same state
+-- to get values of distinct states we use the DISTINCT keyword
