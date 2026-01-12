69137fd563847fff6a2cb04c1e79a69161b82193
Jassayy <jasgandhi2004@gmail.com>
2026-01-12 21:31:07 +0530

    select and from

--- basic.sql
+++ basic.sql
@@ -1,1 +1,9 @@
-USE sql_store
+-- to convey which db we wanna use  
+USE sql_store;
+
+-- select statement 
+-- we use select statement to select columns based on col names
+-- if we want to select all we use *
+SELECT * FROM customers;
+
+-- from (table_name)
