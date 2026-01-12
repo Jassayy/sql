73d9b3f76fb909997448e0ade973665ce7d47a53
Jassayy <jasgandhi2004@gmail.com>
2026-01-12 21:42:23 +0530

    order by used for sorting

--- basic.sql
+++ basic.sql
@@ -4,6 +4,8 @@ USE sql_store;
 -- select statement 
 -- we use select statement to select columns based on col names
 -- if we want to select all we use *
-SELECT * FROM customers;
-
--- from (table_name)
+SELECT *
+FROM customers 
+-- WHERE customer_id = 1 
+ORDER BY last_name;  -- order by is used for sorting based on the column name provided  
+-- from (table_name) WHERE (condition)
