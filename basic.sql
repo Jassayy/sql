4cf192648a277a26a0db8994f215019d57cfb1cb
Jassayy <jasgandhi2004@gmail.com>
2026-01-13 14:12:29 +0530

    ex6

--- basic.sql
+++ basic.sql
@@ -1,11 +1,6 @@
-SELECT * 
-FROM customers 
--- WHERE birth_date > '1990-01-01' AND points > 2000; -- and clause is used for writing multiple conditions
--- will give customers that have both of these conditions true
--- similarly we have the OR clause
--- WHERE birth_date > '1990-01-01' OR points > 2000; -- any one condition can be true in this and it will give the data
--- WHERE birth_date > '1990-01-01' OR points > 1000 AND state = 'VA';
-WHERE NOT (birth_date > '1990-01-01' OR points > 1000 AND state = 'VA'); 
--- Not operator to negate the conditions
+-- get customers whose addresses contain trail or avenue phone nubers end with 9
 
--- these are the logical operators that we can use in sql
+SELECT *
+FROM customers
+-- WHERE address LIKE '%trail%' OR address LIKE '%avenue%';
+WHERE phone LIKE '%9';
